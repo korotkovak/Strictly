80d98e9afdbd24b91403cb108c32620538a9ae0b
korotkovak <kristinakorotkova059@gmail.com>
2023-07-21 23:12:39 +0600

    Initial Commit

---
+++ Strictly/StrictlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrictlyApp.swift
+//  Strictly
+//
+//  Created by Kristina Korotkova on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct StrictlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
